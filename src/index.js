a47747f801e40b746d95b67e1bbe575cbf5ff109
yshepilov <buggygm@gmail.com>
2022-11-06 15:41:04 +0100

    fixed missing imports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import TestApp from '@/TestApp';
-import 'material-design-icons/iconfont/material-icons.css';
-import 'materialize-css/js/autocomplete';
-import 'materialize-css/js/dropdown';
-import 'materialize-css/js/forms';
+import 'materialize-css/sass/ghpages-materialize.scss';
+import './global-imports'
 import 'materialize-css/js/modal';
-import 'materialize-css/js/scrollspy';
-import 'materialize-css/js/select';
+import 'materialize-css/js/dropdown';
 import 'materialize-css/js/sidenav';
 import 'materialize-css/js/tabs';
 import 'materialize-css/js/timepicker';
-import 'materialize-css/sass/ghpages-materialize.scss';
+import 'materialize-css/js/select';
+import 'materialize-css/js/forms';
+import 'materialize-css/js/autocomplete';
+import 'materialize-css/js/scrollspy';
+import 'material-design-icons/iconfont/material-icons.css';
 import Vue from 'vue';
-import './global-imports'
+import TestApp from '@/TestApp';
 
 new Vue({
     render: h => h(TestApp)
