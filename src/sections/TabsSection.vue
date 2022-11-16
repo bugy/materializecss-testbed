bb7cdd6797e7f5b999639c3c2afcbebcc90ec019
yshepilov <buggygm@gmail.com>
2022-11-16 20:00:46 +0100

    added tabs-transparent component

--- src/sections/TabsSection.vue
+++ src/sections/TabsSection.vue
@@ -11,6 +11,16 @@
         </ul>
       </div>
     </div>
+    <div class="row">
+      <div class="col s4">
+        <ul class="tabs z-depth-1 tabs-transparent" style="background-color: green">
+          <li class="tab col s3"><a href="#">Test 1</a></li>
+          <li class="tab col s3"><a class="active" href="#">Test 2</a></li>
+          <li class="tab col s3 disabled"><a href="#">Disabled Tab</a></li>
+          <li class="tab col s3"><a href="#">Test 4</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,8 +28,10 @@
 export default {
   name: 'TabsSection',
   mounted() {
-    const tabs = document.querySelectorAll('.tabs');
-    M.Tabs.init(tabs, {});
+    const normalTabs = document.querySelectorAll('.tabs');
+    M.Tabs.init(normalTabs, {});
+    const transparentTabs = document.querySelectorAll('.tabs.tabs-transparent');
+    M.Tabs.init(transparentTabs, {});
   }
 }
 </script>
